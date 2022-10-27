1f04ea9044c2bafeaa4481ff547afdec89549d4f
char543 <charlie.stuart92@gmail.com>
2022-10-27 04:42:03 +0100

    navbar updates

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,6 +11,7 @@ export const Navbar = () => {
 
   const handleClick = () => {
     setActive(!active);
+    document.body.style.overflow = "hidden";
   };
 
   return (
@@ -21,7 +22,7 @@ export const Navbar = () => {
         </Link>
         {/* menu button */}
         <button
-          className=" inline-flex p-3 hover:bg-green-600 rounded md:hidden text-white ml-auto hover:text-white outline-none"
+          className=" inline-flex p-3 hover:bg-green-600 rounded md:hidden text-white ml-auto hover:text-white outline-none absolute right-3 md:static z-10"
           onClick={handleClick}
         >
           <svg
@@ -41,21 +42,31 @@ export const Navbar = () => {
         </button>
         {/* end button */}
 
-        <div className="text-white">
-          <Link href="/">
-            <a className="m-4">Home</a>
-          </Link>
-          <Link href="/">
-            <a className="m-4">About</a>
-          </Link>
-          <Link href="/">
-            <a className="m-4">Services</a>
-          </Link>
-          <Link href="/">
-            <a className="m-4">Contact</a>
-          </Link>
+        {/* begin ternary div */}
+        {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
+        <div
+          className={`${
+            active ? "" : "hidden"
+          } md:inline-flex md:justify-center m-0 w-40 text-center md:w-full absolute md:static right-0`}
+        >
+          {/* end ternary div */}
+
+          <div className="text-black z-20 md:text-white flex flex-col absolute m-0 w-64 right-0 top-[40px] md:block md:static md:m-auto md:w-full md:h-full h-screen bg-slate-300 rounded-2xl md:bg-transparent">
+            <Link href="/">
+              <a className="m-4">Home</a>
+            </Link>
+            <Link href="/">
+              <a className="m-4">About</a>
+            </Link>
+            <Link href="/">
+              <a className="m-4">Services</a>
+            </Link>
+            <Link href="/">
+              <a className="m-4">Contact</a>
+            </Link>
+          </div>
         </div>
-        <div className="text-white flex flex-row mr-10">
+        <div className="text-white hidden md:flex flex-row mr-10">
           <LinkedInLogoIcon className="m-2 w-5 h-5" />
           <InstagramLogoIcon className="m-2 w-5 h-5" />
           <TwitterLogoIcon className="m-2 w-5 h-5" />
@@ -64,77 +75,3 @@ export const Navbar = () => {
     </>
   );
 };
-
-/*  ./components/Navbar.jsx     */
-// import Link from "next/link";
-// import { useState } from "react";
-
-// export const Navbar = () => {
-//   const [active, setActive] = useState(false);
-
-//   const handleClick = () => {
-//     setActive(!active);
-//   };
-
-//   return (
-//     <>
-//       <nav className=" flex items-center flex-wrap bg-[#1F1F1F] p-3 ">
-//         <Link href="/">
-//           <a className="inline-flex items-center p-2 mr-4 ">
-//             <span className="text-2xl md:text-3xl text-white tracking-wide">
-//               Gig.events
-//             </span>
-//           </a>
-//         </Link>
-//         <button
-//           className=" inline-flex p-3 hover:bg-green-600 rounded lg:hidden text-white ml-auto hover:text-white outline-none"
-//           onClick={handleClick}
-//         >
-//           <svg
-//             className="w-6 h-6"
-//             fill="none"
-//             stroke="currentColor"
-//             viewBox="0 0 24 24"
-//             xmlns="http://www.w3.org/2000/svg"
-//           >
-//             <path
-//               strokeLinecap="round"
-//               strokeLinejoin="round"
-//               strokeWidth={2}
-//               d="M4 6h16M4 12h16M4 18h16"
-//             />
-//           </svg>
-//         </button>
-//         {/*Note that in this div we will use a ternary operator to decide whether or not to display the content of the div  */}
-//         <div
-//           className={`${
-//             active ? "" : "hidden"
-//           }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
-//         >
-//           <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
-//             <Link href="/">
-//               <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white ">
-//                 Home
-//               </a>
-//             </Link>
-//             <Link href="/">
-//               <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">
-//                 Services
-//               </a>
-//             </Link>
-//             <Link href="/">
-//               <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">
-//                 About us
-//               </a>
-//             </Link>
-//             <Link href="/">
-//               <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white font-bold items-center justify-center hover:bg-green-600 hover:text-white">
-//                 Contact us
-//               </a>
-//             </Link>
-//           </div>
-//         </div>
-//       </nav>
-//     </>
-//   );
-// };
